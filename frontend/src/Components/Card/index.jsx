83cb45a48d3003170ba95d753b045f166d3f8fa2
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2024-05-19 15:03:39 +0500

    modal

--- frontend/src/Components/Card/index.jsx
+++ frontend/src/Components/Card/index.jsx
@@ -5,12 +5,13 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import Button from "@mui/material/Button";
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import EditIcon from '@mui/icons-material/Edit';
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
+import { Box, Typography } from "@mui/material";
 
 export default function AccordionUsage() {
   return (
-    <box>
+    <Box>
       <Accordion
         sx={{
           width: 1150,
@@ -21,17 +22,28 @@ export default function AccordionUsage() {
           aria-controls="panel3-content"
           id="panel3-header"
         >
-          Accordion Actions
+          <Typography variant="h6" sx={{ fontWeight: "400" }}>
+            Accordion Actions
+          </Typography>
         </AccordionSummary>
+
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
         </AccordionDetails>
         <AccordionActions>
-          <Button variant="outlined" > <EditIcon /> Edit</Button>
-          <Button variant="contained" color="error"> <DeleteOutlineIcon /> Delete</Button>
+          <Button variant="outlined">
+            {" "}
+            <EditIcon /> Edit
+          </Button>
+          <Button variant="contained" color="error">
+            {" "}
+            <DeleteOutlineIcon /> Delete
+          </Button>
         </AccordionActions>
       </Accordion>
-    </box>
+    </Box>
   );
 }
